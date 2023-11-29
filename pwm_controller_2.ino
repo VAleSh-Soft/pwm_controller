160253520e494246ec9e6bd8ecf6cb1803ca1e5f
VAleSh-Soft <v258@narod.ru>
2023-11-29 22:01:44 +0300

    version 0.2

--- pwm_controller_2.ino
+++ pwm_controller_2.ino
@@ -1,8 +1,17 @@
 /**
  * @file pwm_controller_2.ino
  * @author Vladimir Shatalov (valesh-soft@yandex.ru)
- * @brief Управление нагревателем с помощью ШИМ; текущий уровень мощности индицируется четырьмя светодиодами;
- *        управление уровнем мощности одной кнопкой
+ * @brief Управление нагревателем с помощью ШИМ и управлением одной кнопкой; 
+ * 
+ * Управление: 
+ *   - нагреватель имеет четыре уровня мощности;
+ *   - изменение уровня выполняется кликом кнопкой; перебор уровней закольцован - при максимальном уровне клик кнопкой включает минимальный уровень;
+ *   - удержание кнопки нажатой в течение 2 секунд отключает нагреватель;
+ * 
+ * Индикация:
+ *   - текущий уровень мощности индицируется линейкой из четырех светодиодов;
+ *   - первый светодиод линейки двухцветный, красный цвет зажигается, если нагреватель отключен (pwm_data == 0);
+ * 
  * @version 1.0
  * @date 29.11.2023
  *
@@ -18,22 +27,23 @@ const uint8_t led_1_pin = 3;
 const uint8_t led_2_pin = 4;
 const uint8_t led_3_pin = 5;
 const uint8_t led_4_pin = 6;
+const uint8_t led_off_pin = 8;
 
 const uint8_t pwm_out = 10;
 
 const uint8_t btn_pin = 7;
 
-// считывание сохраненного значения ШИМ
-uint8_t read_pwm_data()
-{
-  return (EEPROM.read(eeprom_index));
-}
+// считывание сохраненного значения ШИМ из EEPROM
+uint8_t read_pwm_data() { return (EEPROM.read(eeprom_index)); }
 
-// сохранение нового значения ШИМ
-void write_pwm_data(uint8_t _data)
-{
-  EEPROM.update(eeprom_index, _data);
-}
+// сохранение нового значения ШИМ в EEPROM
+void write_pwm_data(uint8_t _data) { EEPROM.update(eeprom_index, _data); }
+
+// считывание флага запуска ШИМ
+bool read_on_off_pwm_state() { return ((bool)EEPROM.read(eeprom_index + 1)); }
+
+// запись флага запуска ШИМ
+void write_on_off_pwm_state(bool _flag) { EEPROM.update(eeprom_index + 1, _flag); }
 
 class Heater
 {
@@ -44,9 +54,11 @@ private:
   uint8_t led_2 = led_2_pin;
   uint8_t led_3 = led_3_pin;
   uint8_t led_4 = led_4_pin;
+  uint8_t led_off = led_off_pin;
 
   void set_led_state()
   {
+    digitalWrite(led_off, pwm_data == 0);
     digitalWrite(led_1, pwm_data >= 60);
     digitalWrite(led_2, pwm_data >= 120);
     digitalWrite(led_3, pwm_data >= 180);
@@ -64,11 +76,13 @@ public:
   }
 
   void set_pwm_data(uint8_t _data) { pwm_data = _data; }
+
   uint8_t get_pwm_data() { return pwm_data; }
 
   void tick()
   {
     static uint8_t old_data = 0;
+
     if (old_data != pwm_data)
     {
       old_data = pwm_data;
@@ -78,6 +92,7 @@ public:
       {
         write_pwm_data(pwm_data);
       }
+      write_on_off_pwm_state((bool)pwm_data);
     }
   }
 };
@@ -121,13 +136,16 @@ void setup()
   btn.setLongClickMode(LCM_ONLYONCE);
   btn.setTimeoutOfLongClick(2000);
 
-  // если в памяти записано некорректное значение (0xFF), устанавливаем минимальный уровень
-  if (read_pwm_data() > 240)
+  // если в памяти записано некорректное значение (0xFF или 0x00), устанавливаем минимальный уровень
+  if (read_pwm_data() > 240 || read_pwm_data() == 0)
   {
     write_pwm_data(60);
   }
   // запускаем нагреватель с сохраненным уровнем ШИМ
-  heater.set_pwm_data(read_pwm_data());
+  if (read_on_off_pwm_state())
+  {
+    heater.set_pwm_data(read_pwm_data());
+  }
 }
 
 void loop()
